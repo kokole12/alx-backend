c0cc3b51ce010d022b7155ac199f5ffebf4c865f
kokole12 <kokoleismail886@gmail.com>
2023-07-25 12:34:38 +0300

    task 0 of cachng

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""basic cache"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """initializing from super class"""
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """assigning value to key"""
+        if key is None or item is None:
+            pass
+        self.cache_data[key] = item
+
+    def get(self, key):
+        if key is None or key not in self.cache_data.keys():
+            return None
+        """returning value of cache data"""
+        return self.cache_data.get(key)
