11237e62715b57dfc1e9bfd532df57c667a58b01
kokole12 <kokoleismail886@gmail.com>
2023-07-25 22:30:19 +0300

    fixed pycodestyle errors

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,30 +1,42 @@
 #!/usr/bin/env python3
-"""LIFO cache in python"""
+""" BaseCaching module
+"""
 from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """initialising from the parent class"""
+    """
+    FIFOCache defines a FIFO caching system
+    """
+
     def __init__(self):
+        """
+        Initialize the class with the parent's init method
+        """
         super().__init__()
         self.order = []
 
     def put(self, key, item):
-        """adding item to lifo cache"""
+        """
+        Cache a key-value pair
+        """
         if key is None or item is None:
             pass
-        length = len(self.cache_data)
-        if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
-            print("DISCARD: {}".format(self.order[-1]))
-            del self.cache_data[self.order[-1]]
-            del self.order[-1]
-        if key in self.order:
-            del self.order[self.order.index(key)]
-        self.order.append(key)
-        self.cache_data[key] = item
+        else:
+            length = len(self.cache_data)
+            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                print("DISCARD: {}".format(self.order[-1]))
+                del self.cache_data[self.order[-1]]
+                del self.order[-1]
+            if key in self.order:
+                del self.order[self.order.index(key)]
+            self.order.append(key)
+            self.cache_data[key] = item
 
     def get(self, key):
-        """getting item by key"""
-        if key is None or key not in self.cache_data.keys():
-            return None
-        return self.cache_data.get(key)
+        """
+        Return the value linked to a given key, or None
+        """
+        if key is not None and key in self.cache_data.keys():
+            return self.cache_data[key]
+        return None
