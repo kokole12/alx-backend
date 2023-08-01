f10f36e14f14b00fa3479d2666630ad44b976330
kokole12 <kokoleismail886@gmail.com>
2023-08-01 17:00:28 +0300

    simple flask application

--- 0x02-i18n/app.py
+++ 0x02-i18n/app.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
-"""A simple flask app
-"""
+"""A simple flask application"""
 
 
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
+
 
 app = Flask(__name__)
 
 
+"""route to the index page"""
 @app.route('/', strict_slashes=False)
-async def hello_world() -> str:
-    """_summary_
-    """
-    return await render_template('0-index.html')
+def index() -> str:
+    """rendering the template"""
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run(port="5000", host="0.0.0.0", debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
