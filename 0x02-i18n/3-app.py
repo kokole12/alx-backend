a88b926401bb9bf63c544cf700078e2a9b064213
kokole12 <kokoleismail886@gmail.com>
2023-08-01 18:05:41 +0300

    transilating in template

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+    simple flask application transilated in
+    english and french
+"""
+
+
+from flask import Flask, request
+from flask import render_template
+from flask_babel import Babel
+
+
+class Config(object):
+    """declaring the babel configurations"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """implementing the get best match to get languages support"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """rending the template"""
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
