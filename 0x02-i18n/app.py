e3bb075fd819cb70b19f333b231013855547eba2
kokole12 <kokoleismail886@gmail.com>
2023-08-01 16:46:22 +0300

    removed virtual env

--- 0x02-i18n/app.py
+++ 0x02-i18n/app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 async def hello_world() -> str:
     """_summary_
     """
-    return render_template('0-index.html')
+    return await render_template('0-index.html')
 
 
 if __name__ == '__main__':
