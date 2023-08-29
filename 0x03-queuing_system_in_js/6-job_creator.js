28c879e33e35cc41353e131a498e8482c1212ca0
kokole12 <kokoleismail886@gmail.com>
2023-08-29 14:41:07 +0300

    created the job processor calling sendNotification and updated notification object

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -3,8 +3,8 @@ import { createQueue } from "kue";
 const queue = createQueue()
 
 const notification = {
-    phoneNumber: "",
-    message: ""
+    phoneNumber: "4153518780",
+    message: "This is the code to verify your account"
 }
 
 const job = queue.create('push_notification_code', notification).save(function(error) {
